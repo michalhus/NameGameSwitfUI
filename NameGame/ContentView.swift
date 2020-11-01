c45001c0fe0d055227dad96703d7c54208dc1788
Michal Hus <michalhus@WTA-MBP466.local>
2020-10-31 20:01:44 -0400

    Initial Commit

---
+++ NameGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameGame
+//
+//  Created by Michal Hus on 10/31/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NameGame/NameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameGameApp.swift
+//  NameGame
+//
+//  Created by Michal Hus on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct NameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
